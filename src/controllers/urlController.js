c477e1e10cb34aa19a67c3cff70923f9cc8dbcd3
Lud <instimila@gmail.com>
2022-08-06 16:44:02 +0200

    getLinkById

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -21,3 +21,19 @@ export async function shortLink(req,res){
 res.send({shortURL}).status(200)
 
 }
+
+export async function getLinkById(req,res){
+    const {id}= req.params
+
+    const result= await db.query(` SELECT * FROM urls WHERE id=$1  `, [id])
+    if(result.rowCount==0) return res.sendStatus(404)
+
+    const [link]= result.rows
+
+    delete link.userId;
+    delete link.visitCount;
+
+    res.send(link).status(200)
+    
+
+}
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -2,14 +2,14 @@ import { Router } from "express"
 import authRoute from "../middlewares/authorization.js";
 import { shortLink } from "../controllers/urlController.js";
 import { validateUrl } from "../middlewares/validation.js";
+import { getLinkById } from "../controllers/urlController.js";
 
 const urlRouter= Router()
 
 
 
 urlRouter.post("/urls/shorten",  authRoute , validateUrl, shortLink)
-
-
+urlRouter.get("/urls/:id" , getLinkById)
 
 
 export default urlRouter;
